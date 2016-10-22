9db2349c714b739f8763f9cdfc79d3e15ffd82b5
chrisliu1 <wjliu1998@gmail.com>
2016-10-22 14:54:15 +0800

    write ls with one parameter

---
+++ one_para_ls.c
@@ -0,0 +1,21 @@
+#include "ls.h"
+
+void one_para_ls(){
+	DIR *dp;
+	struct dirent *dirp;
+	get_current_dir();
+	fprintf(stdout, "current dir: %s\n", dir.dirpath);
+	if( (dp = opendir(dir.dirpath)) == NULL)
+		fprintf(stderr, "can't open%s\n", dir.dirpath);
+	while( ( dirp = readdir(dp)) != NULL){
+		if(dirp->d_name[0] =='.')
+			continue;
+		else{
+			fprintf(stdout, "%s  ", dirp->d_name);
+		}
+	}
+	fprintf(stdout, "\n");
+	closedir(dp);
+}
+
+
